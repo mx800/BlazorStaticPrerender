721261a33ec7438e983656f55fa14566c9cb65b7
asda <asdasd>
2025-07-18 21:16:14 -0400

    Test build

--- deploy.ps1
+++ deploy.ps1
@@ -14,3 +14,6 @@ New-Item -ItemType File -Path docs\.nojekyll
 Remove-Item docs\web.config -ErrorAction SilentlyContinue
 Remove-Item -Recurse -Force docs/wwwroot -ErrorAction SilentlyContinue
 Remove-Item docs\BlazorStaticPrerender.staticwebassets.endpoints.json -ErrorAction SilentlyContinue  
+
+# Met à jour le <base href> dans docs/index.html pour GitHub Pages
+(Get-Content docs/index.html) -replace '<base href=\"/\" ?/?>', '<base href=\"/BlazorStaticPrerender/\" />' | Set-Content docs/index.html
